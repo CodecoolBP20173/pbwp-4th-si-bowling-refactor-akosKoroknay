09f688f1c3b1855d3c247ee43df7a71588dd437a
Akos <koroknayakos@gmail.com>
2017-11-26 22:32:31 +0100

    Removed: bad variable names, long methods

--- bowling.py
+++ bowling.py
@@ -1,44 +1,35 @@
 def score(game):
     result = 0
     frame = 1
-    in_first_half = True
+    first_try = True
     for i in range(len(game)):
         if game[i] == '/':
-            result += 10 - last
+            result += 10 - get_value(game[i-1])
         else:
             result += get_value(game[i])
         if frame < 10 and get_value(game[i]) == 10:
             if game[i] == '/':
                 result += get_value(game[i + 1])
-            elif game[i] == 'X' or game[i] == 'x':
+            elif game[i].lower() == 'x':
                 result += get_value(game[i + 1])
                 if game[i + 2] == '/':
                     result += 10 - get_value(game[i + 1])
                 else:
                     result += get_value(game[i + 2])
-        last = get_value(game[i])
-        if not in_first_half:
+        if not first_try or game[i].lower() == 'x':
             frame += 1
-        if in_first_half == True:
-            in_first_half = False
+            first_try = True
         else:
-            in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
-            in_first_half = True
-            frame += 1
+            first_try =  False
     return result
 
 
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
-        return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
-        return 10
-    elif char == '-':
-        return 0
+    values = {'1': 1, '2': 2, '3': 3, '4': 4,\
+    '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'x': 10,\
+    '/': 10, '-': 0}
+    if char.lower() in values:
+        return values[char.lower()]
     else:
         raise ValueError()
+    
